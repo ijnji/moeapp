3c56e0be9a0695f9600663ef9b64fbfdb84a33d7
ijnji <rnduan@gmail.com>
2017-12-23 05:49:28 -0500

    Initial commit.

---
+++ src/test/java/moe/ijnji/MoeAppTest.java
@@ -0,0 +1,33 @@
+package moe.ijnji;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MoeAppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MoeAppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(MoeAppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
