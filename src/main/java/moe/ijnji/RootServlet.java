365c83a0aa64a6c6eba653e2cd4bd13002a7a7cd
Ran Duan <rnduan@gmail.com>
2018-01-24 23:54:18 -0800

    All routes return a single jsp

---
+++ src/main/java/moe/ijnji/RootServlet.java
@@ -0,0 +1,19 @@
+package moe.ijnji;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+@WebServlet(value = "/")
+public class RootServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+
+}
